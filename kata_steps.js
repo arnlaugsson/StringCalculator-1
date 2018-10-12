642c8d65956a973c793efe329f69f2192cc102f8
logisteinn <34041474+logisteinn@users.noreply.github.com>
2018-10-12 12:39:13 +0000

    Nr 5 er komid en er ekki ad fa dad sem eg vill i nr 4

--- kata_steps.js
+++ kata_steps.js
@@ -8,24 +8,64 @@ function Add( numbers ) {
 	
 	if ( numbers.includes(",") || numbers.includes("\n") ) {
 		
-		var numberArr = numbers.split(/,|\n/g);
-		
-		
+		var numberArr = numbers.split(/,|\n/g);	
+			
 		return Sum( numberArr ); 
 	}
-	
 	
 	else {
 		return parseInt( numbers );
 	}
 }
 
+function check_nega ( numbers ) {
+	
+	var negativ = numbers.filter(function(num) {
+		return num < 0; 
+	});
+	
+	if ( negativ.length > 0 ) {
+		throw new Error ( "Negatives Not allowed: " + negativ.join(" , "));
+	}
+	else {
+		return numbers;
+	}
+	
+}
+
+/*function check_nega ( numbers ) {
+	
+	var negArr = [];
+	
+	for ( var i = 0; numberArr.length > i; i++ ) {
+		if ( parseInt( numberArr[i]) < 0 ){
+			negArr = numberArr[i];
+		}
+	}
+	
+	if ( negArr.length > 0 ){
+		window.alert("Negatives not allowed:" + negArr );
+	}
+	else {
+		return 0;
+	}
+}*/
+
 function Sum( numberArr ) {
 	
 	var total = 0;
 	
 	for ( var i = 0; i < numberArr.length; i++ ){
-		total += parseInt( numberArr[i] );
+		if ( numberArr[i] > 0 ){
+		
+			if ( numberArr[i] <= 1000 ) {
+				total += parseInt( numberArr[i] );
+			}
+		}
+		
+		else {
+			throw "Negatives not allowed: " + numberArr[i];
+		}
 	}
 	
 	return total;
